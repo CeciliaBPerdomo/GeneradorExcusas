7ade6ee26e87df6212ea4e38a0d246b2ebd10d74
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2022-09-26 18:32:11 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //   //write your code here
+  //function randomFrase() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let frase = randomWho + " " + randomWhat + " " + randomWhen;
+  document.getElementById("excuse").innerHTML = frase;
 };
+
+//document.getElementById("myBtn").addEventListener("click", randomFrase());
+
+document.getElementById("myBtn").addEventListener("click", function() {
+  //write your code here
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let frase = randomWho + " " + randomWhat + " " + randomWhen;
+  document.getElementById("excuse").innerHTML = frase;
+});
